2d46899125452a9b0f349c196860ddf71ba57155
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-04-24 23:35:33 +0200

    abstract finish OBJ Interfaces next

--- abstract_class/index.php
+++ abstract_class/index.php
@@ -5,7 +5,7 @@ spl_autoload_register(function ($className) {
 });
 
 
-header('Content-Type:text/plain', true);
+//header('Content-Type:text/plain', true);
 //PDO
 $pdo = (new PDOClient('mysql', 'localhost', 'oo_battle', 'root', ''))->connect();
 
@@ -14,7 +14,11 @@ $mysqli = (new MySQLiClient('localhost', 'oo_battle', 'root', ''))->connect();
 
 $ships = $mysqli->select("SELECT * FROM ship")->get();
 
+$handler = $mysqli->getConnection();
+$p = $handler->query("SELECT * FROM ship WHERE id > 2")->fetch_all();
+
+
+var_dump($p);die;
 foreach ($ships as $ship) {
     var_dump($ship->name);
 }
-//var_dump($ships);
