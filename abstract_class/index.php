b200d6774e833d74f01356e209ac54c2b82ef68c
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-04-24 23:20:10 +0200

    idea rm

--- abstract_class/index.php
+++ abstract_class/index.php
@@ -6,10 +6,15 @@ spl_autoload_register(function ($className) {
 
 
 header('Content-Type:text/plain', true);
+//PDO
 $pdo = (new PDOClient('mysql', 'localhost', 'oo_battle', 'root', ''))->connect();
 
-$ships = $pdo->select("SELECT * FROM ship")->get();
+//MySQLiClient
+$mysqli = (new MySQLiClient('localhost', 'oo_battle', 'root', ''))->connect();
+
+$ships = $mysqli->select("SELECT * FROM ship")->get();
 
 foreach ($ships as $ship) {
     var_dump($ship->name);
 }
+//var_dump($ships);
